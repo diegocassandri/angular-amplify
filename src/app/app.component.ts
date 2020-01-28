fce7bc3234afb8be3323a52892637e97f194c613
diego.cassandri <diego.cassandri@senior.com.br>
2020-01-28 17:52:06 -0300

    alter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Amplify, { API } from 'aws-amplify';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'amplify-app';
+  notes = [];
+
+  ngOnInit() {
+    API.get('notes', '/notes', {}).then(data => {
+      this.notes = data;
+      console.log(data);
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,32 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import Amplify from 'aws-amplify';
+import Amplify, { API} from 'aws-amplify';
 import amplify from './aws-exports';
-Amplify.configure(amplify);
+//Amplify.configure(amplify);
+
+
+Amplify.configure({
+  Auth: {
+      // REQUIRED - Amazon Cognito Identity Pool ID
+      identityPoolId: 'us-east-1:def749da-18b6-44c0-accd-898a71868767',
+      // REQUIRED - Amazon Cognito Region
+      region: 'us-east-1',
+      // OPTIONAL - Amazon Cognito User Pool ID
+      userPoolId: 'us-east-1_gGvh3jepV',
+      // OPTIONAL - Amazon Cognito Web Client ID
+      userPoolWebClientId: '3o3h5v2dbm40jdf9qvu5rko3mu',
+  },
+  API: {
+    endpoints: [
+      {
+        name: "notes",
+        endpoint: "https://wmrvb5s3hk.execute-api.us-east-1.amazonaws.com/prod",
+        region: "us-east-1"
+      },
+    ]
+  }
+});
 
 if (environment.production) {
   enableProdMode();
